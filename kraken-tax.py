ebea0d7846e3b0bc3aaf880708e1e0ce4cae683d
Stanislav Zidek <szidek@redhat.com>
2022-02-07 20:21:35 +0100

    dynamic method and filter selection

--- kraken-tax.py
+++ kraken-tax.py
@@ -13,15 +13,9 @@ from collections import deque
 import util
 from util import error, warn, info, debug, set_verbose
 
-def ryear(row):
-    return int(row['time'][0:4])
-
 def rstring(row):
     base, quote = util.parse_pair(row["pair"])
     return f"{row['time'].split()[0]} {row['type']:<4} {float(row['vol']):>11.4f} {base:<5} for {float(row['cost']):>10.4f} {quote:<5} (price {float(row['price']):>10.4f}, fee {float(row['fee']):>9.6f}) ({row['pair']})"
-
-def rprint(row):
-    debug(rstring(row))
 
 class TaxMethod:
     def __init__(self, name, skip_crypto_crypto=False, price_data=None):
@@ -89,9 +83,9 @@ class TaxMethod:
         error("Not implemented (sell)")
 
 class WeightedAverage(TaxMethod):
-    def __init__(self, name, skip_crypto_crypto=False, cc_currency = "ZEUR"):
+    def __init__(self, name, skip_crypto_crypto=False, profit_currency="ZEUR"):
         super().__init__(name, skip_crypto_crypto=skip_crypto_crypto)
-        self.currency = cc_currency
+        self.currency = util.official_currency(profit_currency)
         self.data = {}
 
     def init_pair(self, pair):
@@ -163,46 +157,96 @@ class WeightedAverage(TaxMethod):
             self.data[profit_pair]["profits"][year] = Decimal(0)
         self.data[profit_pair]["profits"][year] += profit
 
+class Filter:
+    def match(self, typ, day, base, quote, vol, cost, fee):
+        error(f"Filter not implemented")
+
+class FilterOnlyBTCEUR(Filter):
+    def __init__(self):
+        self.name = "btceur"
+
+    def match(self, typ, day, base, quote, vol, cost, fee):
+        return base not in ("XBT", "XXBT") or quote not in ("EUR", "ZEUR")
 
 def main():
     last_year = int(datetime.datetime.now().date().strftime("%Y")) - 1
+    INFILE = "trades.csv"
     PROFIT_CURRENCY = "EUR"
     TAX_CURRENCY = "CZK"
     TAX_DEFAULT = 15
 
+    FILTERS = []
+    btceur = FilterOnlyBTCEUR()
+    FILTERS = [btceur]
+
     ap = argparse.ArgumentParser(description="Compute taxes")
     ap.add_argument("-v", "--verbose", action="store_true",
                     help="verbose - print debug messages")
+    ap.add_argument("-i", "--infile", default=INFILE,
+                    help="verbose - print debug messages")
     ap.add_argument("-y", "--year", default=last_year,
                     help="tax year, last year by default")
-    ap.add_argument("-c", "--currency", default=PROFIT_CURRENCY,
+    ap.add_argument("--profit-currency", default=PROFIT_CURRENCY,
                     help=f"fiat currency for crypto-crypto trades, default {PROFIT_CURRENCY}")
     ap.add_argument("-t", "--tax", default=TAX_DEFAULT,
                     help=f"tax percents, default {TAX_DEFAULT}")
-    ap.add_argument("-p", "--tax-currency", default=TAX_CURRENCY,
+    ap.add_argument("--tax-currency", default=TAX_CURRENCY,
                     help=f"tax currency, default {TAX_CURRENCY}")
+    ap.add_argument("-m", "--method", action="append", dest="methods",
+                    help=f"methods to use, default is all")
+    ap.add_argument("-f", "--filter", action="append", dest="filters",
+                    help=f"Available filters: {', '.join(f.name for f in FILTERS)}; default is none")
 
     args = ap.parse_args()
     
     if args.verbose:
         set_verbose()
 
-    tax_year = int(args.year)
-    tax_currency = args.tax_currency
-    tax = Decimal(0.01) * Decimal(args.tax)
+    infile = args.infile
+    if infile == "-":
+        infile = None
 
-    infile = "trades.csv"
+    tax_year = int(args.year)
+    tax = Decimal(0.01) * Decimal(args.tax)
+    tax_currency = args.tax_currency
+    profit_currency = args.profit_currency
+
+    METHODS = []
+    wa_simple = WeightedAverage("WAsimple", skip_crypto_crypto=True, profit_currency=profit_currency)
+    wa_full = WeightedAverage("WA", skip_crypto_crypto=False, profit_currency=profit_currency)
+    METHODS = [wa_simple, wa_full]
+
+    method_names = args.methods
+    methods = []
+
+    if method_names:
+        debug(f"Method names: {method_names}")
+        for m in METHODS:
+            if m.name in method_names:
+                debug(f"Method to use: {m.name}")
+                methods.append(m)
+            else:
+                debug(f"Method '{m.name}' NOT in {method_names}")
+    if not methods:
+        debug(f"No methods, using all")
+        methods = METHODS
+
+    filters = []
+    if not args.filters:
+        debug(f"No filters")
+    else:
+        for f in FILTERS:
+            if f.name in args.filters:
+                filters.append(f)
+        debug(f"Resulting filters: {', '.join(f.name for f in filters)}")
+
     if infile:
-        print(f"Opening {infile}")
+        debug(f"Opening {infile}")
         f = open(infile, mode="r")
     else: # use stdin
         f = sys.stdin
 
     reader = csv.DictReader(f)
-
-    wa_simple = WeightedAverage("WA-simple", skip_crypto_crypto=True)
-    wa_cc = WeightedAverage("WA-honest", skip_crypto_crypto=False)
-    methods = [wa_simple, wa_cc]
 
     n = 0
     for row in reader:
@@ -220,6 +264,11 @@ def main():
         cost = Decimal(row["cost"])
         vol = Decimal(row["vol"])
         fee = Decimal(row["fee"])
+
+        for f in filters:
+            if f.match(typ, day, base, quote, vol, cost, fee):
+                debug(f"Skipping: {typ} {day} {base} {quote} {vol} {cost} {fee}")
+                continue
 
         for m in methods:
             m.handle(typ, day, base, quote, vol, cost, fee)
@@ -242,7 +291,7 @@ def main():
             if profit:
                 base, quote = (util.human_currency(c) for c in util.parse_pair(pair))
                 total[currency] += profit
-                info(f"    {base:<5} / {quote:<5} {tax_year} profit {profit:>12.4f} {currency}")
+                info(f"    {base:<4} / {quote:<4} {tax_year} profit {profit:>12.2f} {currency}")
         info(f"  PROFITS:")
         tax_base = Decimal(0)
         for k, v in total.items():
@@ -252,10 +301,10 @@ def main():
             p = util.human_currency(k) + util.human_currency(tax_currency)
             tax_sub = forex_prices[tax_year][p] * v
             tax_base += tax_sub
-            info(f"{tax_sub:>22.2f} {tax_currency} <- {v:>12.4f} {k}")
-        info(f"  TOTAL: {tax_base:>13.2f} {tax_currency}")
+            info(f"{tax_sub:>20.2f} {tax_currency}  <-  {v:>10.2f} {k}")
+        info(f"  TOTAL: {tax_base:>11.2f} {tax_currency}")
         tax_total = tax_base * tax
-        info(f"  TAX: {tax_total:>15.2f} {tax_currency}")
+        info(f"  TAX: {tax_total:>13.2f} {tax_currency}")
 
 if __name__ == "__main__":
     sys.exit(main())
